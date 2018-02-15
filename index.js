a7fa5afa4fa915364e38dda0aab7310cf33f9c40
vagrant <spencer.greff@gmail.com>
2018-02-15 20:59:06 +0000

    first commit

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  sum = 0;
+  for (item of array) {
+    if (Array.isArray(item)) {
+      console.log('is array');
+      
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+  }
+  return sum;
 }
+
+console.log(sumItems([1, 2, 3, 4, 5, [6, 7]] ));
 
 module.exports = sumItems;
